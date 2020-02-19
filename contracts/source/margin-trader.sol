67940a77b63c80e7e5307a8eb564e884ef6e7fe8
Micah Zoltu <micah@zoltu.net>
2020-02-19 18:16:11 +0800

    Tests of contract functional.
    Includes deployment to deterministic address.

--- contracts/source/margin-trader.sol
+++ contracts/source/margin-trader.sol
@@ -1,5 +1,7 @@
 pragma solidity 0.6.3;
 
 contract MarginTrader {
-	
+	function greeting() external pure returns(string memory) {
+		return "hello";
+	}
 }
--- test/index.ts
+++ test/index.ts
@@ -1,12 +1,24 @@
-import Jasmine = require('jasmine');
+import Jasmine = require('jasmine')
 const jasmine = new Jasmine({})
 jasmine.randomizeTests(false)
 
 import { Crypto } from '@peculiar/webcrypto'
 (global as any).crypto = new Crypto()
 
-it('works', async () => {
-	expect(true).toBeTruthy()
+import { createMemoryRpc } from './libraries/rpc-factories'
+import { deployMarginTrader } from './libraries/deploy-margin-trader'
+import { MarginTrader } from './generated/margin-trader'
+import { DependenciesImpl } from './libraries/dependencies'
+
+const jsonRpcEndpoint = 'http://localhost:1235'
+const gasPrice = 10n*8n
+
+it('deploys', async () => {
+	const rpc = await createMemoryRpc(jsonRpcEndpoint, gasPrice)
+	const marginTraderAddress = await deployMarginTrader(rpc)
+	const marginTrader = new MarginTrader(new DependenciesImpl(rpc), marginTraderAddress)
+	const greeting = await marginTrader.greeting_()
+	expect(greeting).toEqual('hello')
 })
 
 jasmine.execute()
