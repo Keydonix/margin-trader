14423964a34fe690750601daf58935af23a71537
Scott Bigelow <epheph@gmail.com>
2020-04-16 16:58:38 -0700

    Missed local change

--- contracts/source/MarginTrader.sol
+++ contracts/source/MarginTrader.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.3;
 import "./UniswapOracle.sol";
 
 contract MarginTrader is UniswapOracle {
-	constructor(IUniswapV2Pair _uniswapV2Pair) public UniswapOracle(_uniswapV2Pair) {
+	constructor(IUniswapV2Pair _uniswapV2Pair) public UniswapOracle(_uniswapV2Pair, 1) {
 	}
 
 	function increasePosition(uint256 leverage, uint256 amount, bytes memory historicBlock, bytes memory accountNodesRlp, bytes memory reserveTimestampProofNodesRlp, bytes memory price0ProofNodesRlp) public view returns (string memory) {
